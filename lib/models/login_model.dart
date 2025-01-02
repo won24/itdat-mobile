5cf0369b404f491d3b4ed89397eaf65cde19e230
nicdkim <zzsehdrb@naver.com>
2025-01-02 17:45:40 +0900

    naver,kakao seucess / google progress

--- lib/models/login_model.dart
+++ lib/models/login_model.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
-class LoginModel {
+class LoginModel extends ChangeNotifier{
   //final String baseUrl = 'http://112.221.66.174:8001'; // 원
    final String baseUrl = 'http://10.0.2.2:8082';     // 김
   // final String baseUrl = 'http://112.221.66.174:8000'; // son
