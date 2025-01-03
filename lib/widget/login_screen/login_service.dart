abe9a7a48997741ff4e904aaf1a79a1e62397610
nicdkim <zzsehdrb@naver.com>
2025-01-03 11:32:30 +0900

    login

--- lib/widget/login_screen/login_service.dart
+++ lib/widget/login_screen/login_service.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import '../../models/login_model.dart';
 
@@ -9,21 +8,22 @@ class AuthService {
   // 로그인 메서드
   Future<bool> login(String email, String password) async {
     print("서비스쪽: email = $email, password = $password");
+
     Map<String, String> requestBody = {
-      'email': "sksksk4502@naver.com",
-      'password': "son4970",
+      'email': email,
+      'password': password,
     };
+
     var result = await _loginModel.login(requestBody);
 
     // 로그인 성공 시
     if (result['success']) {
       try {
-        // 서버에서 받은 토큰을 읽어서 안전한 저장소에 저장
         var token = result['data']['token'];
         if (token != null) {
           await storage.write(key: 'auth_token', value: token);
           await storage.write(key: 'email', value: email);
-          print("토큰,이메일  저장 완료");
+          print("토큰, 이메일 저장 완료");
           return true;
         } else {
           print("토큰이 없습니다.");
@@ -51,16 +51,4 @@ class AuthService {
       return false;
     }
   }
-
-  // 로그인 여부 확인 메서드
-  // Future<bool> isLoggedIn() async {
-  //   try {
-  //     // 안전한 저장소에서 토큰을 읽어서 로그인 상태 확인
-  //     String? token = await storage.read(key: 'auth_token');
-  //     return token != null;
-  //   } catch (e) {
-  //     print("로그인 여부 확인 중 에러 발생: $e");
-  //     return false;
-  //   }
-  // }
 }
