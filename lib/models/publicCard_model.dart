ccaf43c2d426825d826f0f2ab0fc6245535f5f6e
nicdkim <zzsehdrb@naver.com>
2025-01-15 13:05:03 +0900

    baseURL

--- lib/models/publicCard_model.dart
+++ lib/models/publicCard_model.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'BusinessCard.dart';
 
 class PublicCardModel {
-
+  final baseUrl = dotenv.env['BASE_URL'];
   // final String baseUrl = "http://10.0.2.2:8082/card/public";
-   final String baseUrl = "http://112.221.66.174:8001/card/public";
+  //  final String baseUrl = "http://112.221.66.174:8001/card/public";
 
   //final String baseUrl = "http://10.0.2.2:8082/card/public";
   // final String baseUrl = "http://112.221.66.174:8001/card/public";
@@ -16,10 +16,9 @@ class PublicCardModel {
   // 전체 공개 명함 가져오기
   Future<List<dynamic>> getAllPublicCards() async {
     try {
-      final response = await http.get(Uri.parse("$baseUrl/all"));
+      final response = await http.get(Uri.parse("$baseUrl/card/public/all"));
 
       if (response.statusCode == 200) {
-        // print("응답 성공: ${response.body}");
         return jsonDecode(utf8.decode(response.bodyBytes));
       } else {
         print("응답 실패: ${response.statusCode}");
@@ -40,7 +39,7 @@ class PublicCardModel {
   }) async {
     try {
       final response = await http.post(
-        Uri.parse("$baseUrl/update"),
+        Uri.parse("$baseUrl/card/public/update"),
         headers: {"Content-Type": "application/json"},
         body: jsonEncode({
           "userEmail": userEmail,
